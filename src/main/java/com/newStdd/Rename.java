94d04556c6c50e52768a57e891f19ae346db5e32
doul <doulcn@live.cn>
2013-07-25 10:41:02 +0800

    add rename

---
+++ src/main/java/com/newStdd/Rename.java
@@ -0,0 +1,17 @@
+package com.newStdd;
+
+import java.io.File;
+import java.text.ParseException;
+
+import com.newStdd.util.DateUtil;
+
+public class Rename {
+	public static void main(String[] args) throws ParseException {
+		File directory= new File("d:\\dcim");
+		File[] files= directory.listFiles();
+		for (File file: files) {
+			System.out.println(file.getName()+ ": "+ DateUtil.getDateString(file.lastModified(), DateUtil.dateFormatYyyy_MM_dd_HH_mm_ss));
+			file.renameTo(new File("junk.dat"));
+		}
+	}
+}
