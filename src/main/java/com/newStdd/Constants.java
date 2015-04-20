e7c7deedaf4bb06c32b70fb4501cb6fa4a823438
doul <doulcn@163.com>
2015-04-20 17:39:56 +0800

    完善rename.java

--- src/main/java/com/newStdd/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.newStdd;
-
-public class Constants {
-	public static String sucess = "成功";
-	public static String fail = "失败";
-}
--- src/main/java/com/newStdd/util/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.newStdd.util;
-
-public class Constants {
-	public static String SFileExist = "文件已存在";
-	public static String SRootNotExist = "根目录不存在";
-}
