ea28fe7aa7c67da9c4c46ac30b4d8fef6c33d318
doul <doulcn@163.com>
2015-04-20 17:38:45 +0800

    完善rename.java

---
+++ src/main/java/com/newStdd/io/RootNotFoundException.java
@@ -0,0 +1,7 @@
+package com.newStdd.io;
+
+import com.newStdd.lang.Exception;
+
+public class RootNotFoundException extends Exception {
+	private static final long serialVersionUID = 1230756737473142564L;
+}
---
+++ src/main/java/com/newStdd/lang/Exception.java
@@ -0,0 +1,6 @@
+package com.newStdd.lang;
+
+
+public class Exception extends java.lang.Exception {
+	private static final long serialVersionUID = -4712793155625889700L;
+}
